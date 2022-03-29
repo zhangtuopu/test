d6cfdb6a34624b0756c00b28e1b44b01b955a96c
zhangtuopu <672049157@qq.com>
2022-03-29 16:15:02 +0800

    20220329

--- study_cpp/src/main.cpp
+++ study_cpp/src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
-
+#include <string>
+#include <sstream>
+#include <vector>
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> vec{1,2,4,8,13,17,20};
+    int target=7;
+    auto vbegin=vec.begin(),vend=vec.end();
+    auto vmid=vbegin+(vend-vbegin)/2;
+    while(vmid!=vend && *vmid!=target)
+    {
+        if (target<*vmid)
+        {
+            vend=vmid;
+        }
+        else
+        {
+            vbegin=vmid+1;
+        }
+        vmid=vbegin+(vend-vbegin)/2;
+    }
+
+    int tt=0;
+    std::cout<<tt<<++tt<<std::endl;
     return 0;
 }
